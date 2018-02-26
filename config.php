94843565fe3f1d9ae5c4916dde293c47d9f4da48
Benjamin Measures <saint.abroad@gmail.com>
2018-02-26 17:40:16 +0000

    Read config from non-option arguments

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Commando\Command;
+use Bmea\Config;
+
+$cmd = new Command();
+
+$cmd->option('t')
+    ->aka('template')
+    ->describedAs('Process template FILE')
+    ->expectsFile();
+
+# merge config from non-option arguments
+$conf = new Config($cmd->getArgumentValues());
+
+echo json_encode($conf->all()), PHP_EOL;
---
+++ src/Config.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Bmea;
+
+class Config extends \Noodlehaus\Config
+{
+}
