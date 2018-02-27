32caa33fd49511bf4c09293c8d80848e08762a06
Benjamin Measures <saint.abroad@gmail.com>
2018-02-27 18:00:25 +0000

    Explode dot-notation config

--- configurator.php
+++ configurator.php
@@ -19,5 +19,23 @@ $tpl = $m->loadTemplate(file_get_contents($cmd['template']));
 # merge config from non-option arguments
 $conf = new Noodlehaus\Config($cmd->getArgumentValues());
 
+# explode dot-notation
+function explodeDotNotation($arr) {
+    foreach ($arr as $k => $v) {
+        $keys = explode('.', $k);
+        $r = &$arr;
+        foreach ($keys as $key) {
+            $r = &$r[$key];
+        }
+        $r = $v;
+
+        if (is_array($v)) {
+            $r = explodeDotNotation($v);
+        }
+    }
+    return $arr;
+}
+$conf = explodeDotNotation($conf->all());
+
 # render template
-echo $tpl->render($conf->all());
+echo $tpl->render($conf);
